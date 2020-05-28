79d0894837c03bbb3e5d8b21f3fa1ec1aa6e2a88
davidwa2 <davidwa2@illinois.edu>
2020-05-27 21:35:59 -0400

    Inital file upload: Data parsing complete

---
+++ data_parser.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import re
+from objects import *
+
+#Input: pandas data frame from colony_data
+#Output: Vector of mouse objects and vector of cage objects
+def gen_objs(df):
+    mice = []      #empty vector of mouse objects
+    cages = []     #empty vector of cage objects
+    col_list = df.columns.tolist()
+
+#Construct and assign mouse vectors to cages
+    c_ls = df['Cage ID'].tolist()      #list of all cages including repeats
+    m_ls = df['Mouse ID'].tolist()      #list of all mouse IDs
+
+    #Loop to generate new cage based on new Cage ID
+    #print(c_ls)
+    prev_cage = c_ls[0]
+    new_cage = cage(str(c_ls[0]))
+    self_mice = []
+    for i in range(len(c_ls)):
+        if c_ls[i] != prev_cage:
+            new_cage.mice = self_mice
+            new_cage.total = len(self_mice)
+            cages.append(new_cage)
+            new_cage = cage(str(c_ls[i]))
+            self_mice = []      #vector of mice specific to one cage
+            prev_cage = str(c_ls[i])
+
+        self_mice.append(str(m_ls[i]))
+
+    #set members and append again for final cage outside loop
+    new_cage.mice = self_mice
+    new_cage.total = len(self_mice)
+    cages.append(new_cage)
+    for c in cages:
+        print(c.CID)
+
+    #Loop to initalize all mice objects
+    for i in range(len(m_ls)):
+        new_mouse = mouse(str(m_ls[i]))
+        new_mouse.CID = c_ls[i]
+        if df['Ear Tag?'][i] == 'No':
+            new_mouse.ET = False
+        if df['Sex'][i] == 'M':     #False: Female, True: Male
+            new_mouse.sex = True
+        new_mouse.age = df['Age (days)'][i]
+        if df['Pregnant?'][i] == 'Yes':
+            new_mouse.pregnant = True
+        new_mouse.sacked = df['Sacked Status (Blank, Potential, Sacked)'][i]   #Blank, may be sacked (Potential), or already sacked (Sacked)
+        new_mouse.DOS = df['Date of Sack'][i]
+        if df['Genotyped?'][i] == 'Yes':
+            new_mouse.genotyped = True
+        if df['Runt?'][i] == 'Yes':
+            new_mouse.runt = True
+        new_mouse.comment = df['Comments'][i] #Comment entry
+        mice.append(new_mouse)
+
+    #print(col_list)
+    #print(ID)
+    #print(cages)
+    return mice, cages
+
+#This function fills in the remaining info for the cage objects from the second excel sheet
+def finish_cages(df, cages):
+    for i in range(len(cages)):
+        if df['Breeding?'][i] == 'Yes':
+            cages[i].breeding = True
+        cages[i].EC = df['Experimental Condition'][i]
+        cages[i].pups = df['# of Pups'][i]
+        cages[i].DOB = df['Pup DOB'][i]
+        cages[i].WD = df['Wean Date (DOB + 28 d)'][i]
+    return cages
+
+def parse_data():
+    #data frame generation (skip first row)
+    file = open('dummy_data.xlsx', 'rb')
+    mice_data = pd.read_excel(file, sheet_name = 'Mice', skiprows = 1)
+    #usecols = ['Mouse ID', 'Cage ID', 'Ear Tag?', etc.]
+    cage_data = pd.read_excel(file, sheet_name = 'Cages', skiprows = 1)
+
+    mice, cages = gen_objs(mice_data)
+    final_cages = finish_cages(cage_data, cages)
+
+    return mice, final_cages
+
+m,f = parse_data()
---
+++ objects.py
@@ -0,0 +1,24 @@
+class mouse:
+    def __init__(self, ID):
+         self.ID = ID   #Mouse ID (string)
+         self.CID = ''  #Cage ID
+         self.ET = True    #Eartag?
+         self.sex = False   #False: Female, True: Male
+         self.age = 0
+         self.pregnant = False
+         self.sacked = ''   #Blank, may be sacked (Potential), or already sacked (Sacked)
+         self.DOS = ''     #Date of Sack
+         self.genotyped = False
+         self.runt = False
+         self.comment = ''  #Comment entry
+
+class cage:
+    def __init__(self, CID):
+        self.CID = CID   #Cage ID
+        self.mice = []  #vector of mouse ID strings in this cage
+        self.total = 0    #Total number of mice in the cage
+        self.breeding = False
+        self.EC = ''    #Experimental Condition
+        self.pups = 0   #Number of pups
+        self.DOB = ''   #Pup date of birth
+        self.WD = ''    #Wean date
