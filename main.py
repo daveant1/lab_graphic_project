57ef7e7fad674a1fa4b912036aafe197db55177c
davidwa2 <davidwa2@illinois.edu>
2020-06-03 11:47:04 -0400

    Added image conversion from eps to png, still must fix scaling

--- main.py
+++ main.py
@@ -35,4 +35,33 @@ for c in cages:
         y1 = y2
         y2 += 160
         row_counter = 0
+win.postscript(file = 'image.eps', colormode = 'color')
+# from PIL import Image
+# output = Image.open("05-28-20.eps")
+# #convert = output.convert('RGBA')
+# output.save("image.png")
+from PIL import Image
+
+TARGET_BOUNDS = (720, 480)
+
+# Load the EPS at 10 times whatever size Pillow thinks it should be
+# (Experimentaton suggests that scale=1 means 72 DPI but that would
+#  make 600 DPI scale=8⅓ and Pillow requires an integer)
+pic = Image.open('image.eps')
+pic.load(scale=10)
+
+# Ensure scaling can anti-alias by converting 1-bit or paletted images
+# if pic.mode in ('P', '1'):
+#     pic = pic.convert("RGB")
+
+# Calculate the new size, preserving the aspect ratio
+ratio = min(TARGET_BOUNDS[0] / pic.size[0],
+            TARGET_BOUNDS[1] / pic.size[1])
+new_size = (int(pic.size[0] * ratio), int(pic.size[1] * ratio))
+
+# Resize to fit the target size
+pic = pic.resize(new_size, Image.ANTIALIAS)
+
+# Save to PNG
+pic.save("image.png")
 win.getMouse()
