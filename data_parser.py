ae4433dcd6447b6ebf63eae06e452c6d754d0e83
davidwa2 <davidwa2@illinois.edu>
2020-07-05 17:45:01 -0400

    Fixed age type error and created idx member for mouse object

--- data_parser.py
+++ data_parser.py
@@ -1,4 +1,8 @@
+###EDITS: made it so blank rows are skipped. fixed issue with age conversion to integer
 import pandas as pd
+import numpy as np
+import xlrd as xd
+from time import perf_counter
 from objects import *
 
 #Input: pandas data frame from colony_data
@@ -6,7 +10,7 @@ from objects import *
 def gen_objs(df):
     mice = {}      #empty dict of mouse objects
     cages = []     #empty vector of cage objects
-    col_list = df.columns.tolist()
+    #col_list = df.columns.tolist()
 
 #Construct and assign mouse vectors to cages
     c_ls = df['Cage ID'].tolist()      #list of all cages including repeats
@@ -20,13 +24,12 @@ def gen_objs(df):
         CID = str(c_ls[i])    #Cage ID
         if CID != prev_cage:
             new_cage.mice = self_mice
-            new_cage.total = len(self_mice)
             cages.append(new_cage)
             new_cage = cage(CID)
             self_mice = []      #vector of mice specific to one cage
             prev_cage = CID
 
-        self_mice.append(str(m_ls[i]))
+        self_mice.append(i)
 
     #set members and append for final cage
     new_cage.mice = self_mice
@@ -36,22 +39,24 @@ def gen_objs(df):
     #Loop to initalize all mice objects
     for i in range(len(m_ls)):
         new_mouse = mouse(str(m_ls[i]))
+        new_mouse.idx = i
         new_mouse.CID = str(c_ls[i])
-        if df['Ear Tag?'][i] == 'NO':
+        if df['Ear Tag?'][i] == 'N':
             new_mouse.ET = False
         if df['Sex'][i] == 'M':     #False: Female, True: Male
             new_mouse.sex = True
-        new_mouse.age = int(df['Age (days)'][i])
-        if df['Pregnant?'][i] == 'YES':
-            new_mouse.pregnant = True
-        new_mouse.sacked = df['Sacked Status (Blank, Potential, Sacked)'][i]   #Blank, may be sacked (Potential), or already sacked (Sacked)
-        new_mouse.DOS = df['Date of Sack'][i]
-        if df['Genotyped?'][i] == 'YES':
+        if not isinstance(df['Age (days)'][i], float):
+            new_mouse.age = df['Age (days)'][i]
+        # if df['Pregnant?'][i] == 'Y':
+        #     new_mouse.pregnant = True
+        new_mouse.sacked = df['Sacked Status: Potential (P), Sacked (S)'][i] #Blank, may be sacked (Potential), or already sacked (Sacked)
+        #new_mouse.DOS = df['Date of Sack'][i]
+        if df['Genotyped?'][i] == 'Y':
             new_mouse.genotyped = True
-        if df['Runt?'][i] == 'YES':
+        if df['Runt?'][i] == 'Y':
             new_mouse.runt = True
-        new_mouse.comment = df['Comments'][i] #Comment entry
-        mice[str(m_ls[i])] = new_mouse
+        #new_mouse.comment = df['Comments'][i] #Comment entry
+        mice[i] = new_mouse
     return mice, cages
 
 #This function fills in the remaining info for the cage objects from the second excel sheet
@@ -60,23 +65,26 @@ def gen_objs(df):
 def finish_cages(df, cages):
     for i in range(len(cages)):
         cages[i].status = df['Status/Condition'][i]
-        # print(type(cages[i].status))
         cages[i].pups = df['Number of Pups'][i]
         cages[i].DOB = df['Pup DOB'][i]
-        cages[i].WD = df['Wean Date (DOB + 28 d)'][i]
     return cages
 
 def parse_data(filename):
-    file = open(filename, 'rb')
+    file = open('../'+filename, 'rb')
     mice_data = pd.read_excel(file, sheet_name = 'Mice', skiprows = 1)  #data frame generation (skip first row)
-    #usecols = ['Mouse ID', 'Cage ID', 'Ear Tag?', etc.]
+    #drop blank rows if any and reset indices
+    mice_data.dropna(axis=0, how = 'all', inplace=True)
+    mice_data.reset_index(drop = True, inplace = True)
     cage_data = pd.read_excel(file, sheet_name = 'Cages', skiprows = 1)
-
-
+    cage_data.dropna(axis=0, how = 'all', inplace=True)
+    cage_data.reset_index(drop = True, inplace = True)
+    # start = perf_counter()
     mice, cages = gen_objs(mice_data)
     final_cages = finish_cages(cage_data, cages)
+    # end = perf_counter()
+    # print('parsing time: ', end-start)
     # for c in final_cages:
     #     print(vars(c))
     # for m in mice.keys():
-    #     print(vars(mice[m]))
+    #     print(type(mice[m].age))
     return mice, final_cages
