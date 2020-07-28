1e982daedbd6c5b241634638b8ae26f58648203e
daveant1 <dopalini@gmail.com>
2020-07-28 19:41:29 -0400

    refactor cage attribute parsing

--- data_parser.py
+++ data_parser.py
@@ -1,36 +1,42 @@
 import pandas as pd
 from objects import *
 
-#Input: pandas data frame from colony_data
-#Output: Vector of mouse objects and vector of cage objects
-def gen_objs(df):
+#Input: pandas data frames df (mouse data) and df2 (cage data)
+#Output: dict of mouse objects and vector of cage objects
+def gen_objs(df, df2):
     mice = {}      #empty dict of mouse objects
     cages = []     #empty vector of cage objects
-    #col_list = df.columns.tolist()
 
 #Construct and assign mouse vectors to cages
     c_ls = df['Cage ID'].tolist()      #list of all cages including repeats
     m_ls = df['Mouse ID'].tolist()      #list of all mouse IDs
 
-    #Loop to generate new cage entry based on new Cage ID
+    #Loop to generate new cage entry based on new CID
     prev_cage = str(c_ls[0])
     new_cage = cage(str(c_ls[0]))
     self_mice = []
+
     for i in range(len(c_ls)):
-        CID = str(c_ls[i])    #Cage ID
-        if CID != prev_cage:
+        CID = str(c_ls[i])
+
+        if CID != prev_cage:    #Append new cage and reset vector
             new_cage.mice = self_mice
             cages.append(new_cage)
             new_cage = cage(CID)
-            self_mice = []      #vector of mice specific to one cage
+            self_mice = []
             prev_cage = CID
 
-        self_mice.append(i)
+        self_mice.append(i)     #Append mouse idx
 
-    #set members and append for final cage
+    #Construct and append final cage
     new_cage.mice = self_mice
-    new_cage.total = len(self_mice)
     cages.append(new_cage)
+
+    #Update remaining cage attributes
+    for i in range(len(cages)):
+        cages[i].status = df2['Status/Condition'][i]
+        cages[i].pups = df2['Number of Pups'][i]
+        cages[i].DOB = df2['Pup DOB'][i]
 
     #Loop to initalize all mice objects
     for i in range(len(m_ls)):
@@ -45,43 +51,29 @@ def gen_objs(df):
             new_mouse.age = df['Age (days)'][i]
         if str(df['Pregnant?'][i]).lower() in ('y', 'yes'):
             new_mouse.pregnant = True
-        new_mouse.sacked = str(df['Sacked Status: Potential (P), Sacked (S)'][i]).lower() #Blank, may be sacked (Potential), or already sacked (Sacked)
-        #new_mouse.DOS = df['Date of Sack'][i]
+        new_mouse.sacked = str(df['Sacked Status: Potential (P), Sacked (S)'][i]).lower() #Blank, potential for sack (p), or already sacked (s)
         if str(df['Genotyped?'][i]).lower() in ('y', 'yes'):
             new_mouse.genotyped = True
         if str(df['Runt?'][i]).lower() in ('y', 'yes'):
             new_mouse.runt = True
-        #new_mouse.comment = df['Comments'][i] #Comment entry
         mice[i] = new_mouse
+
     return mice, cages
 
-#This function fills in the remaining info for the cage objects from the second excel sheet
-#Input: pandas data frame of cages sheet, unfinished vector of cages
-#Output: finished vector of cages
-def finish_cages(df, cages):
-    for i in range(len(cages)):
-        cages[i].status = df['Status/Condition'][i]
-        cages[i].pups = df['Number of Pups'][i]
-        cages[i].DOB = df['Pup DOB'][i]
-    return cages
 
 def parse_data(filename):
-    file = open('../'+filename, 'rb')
-    mice_data = pd.read_excel(file, sheet_name = 'Mice', skiprows = 1)  #data frame generation (skip first row)
-    #drop blank rows if any and reset indices
-    mice_data.dropna(axis=0, how = 'all', inplace=True)
+    file = open('../' + filename, 'rb')
+
+    mice_data = pd.read_excel(file, sheet_name = 0, skiprows = 1)  #data frame generation (skip first row)
+    mice_data.dropna(axis=0, how = 'all', inplace = True)   #drop blank rows and reset indices
     mice_data.reset_index(drop = True, inplace = True)
-    cage_data = pd.read_excel(file, sheet_name = 'Cages', skiprows = 1)
+
+    cage_data = pd.read_excel(file, sheet_name = 1, skiprows = 1)
     cage_data.dropna(axis=0, how = 'all', inplace=True)
     cage_data.reset_index(drop = True, inplace = True)
-    # start = perf_counter()
-    mice, cages = gen_objs(mice_data)
-    final_cages = finish_cages(cage_data, cages)
-    final_cages.sort(key = lambda x: str(x.status))
-    # end = perf_counter()
-    # print('parsing time: ', end-start)
-    # for c in final_cages:
-    #     print(vars(c))
-    # for m in mice.keys():
-    #     print(vars(mice[m]))
-    return mice, final_cages
+
+    #Construct and sort mice/cage objects
+    mice, cages = gen_objs(mice_data, cage_data)
+    cages.sort(key = lambda x: str(x.status))
+
+    return mice, cages
