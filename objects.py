c3d6a4c3850b3e15000d4ee51ab62bc2ed63bd05
daveant1 <dopalini@gmail.com>
2020-07-28 19:38:52 -0400

    Removed unnecessary object git attributes/fields

--- objects.py
+++ objects.py
@@ -1,22 +1,21 @@
 class mouse:
     def __init__(self, ID):
-         self.ID = ID   #Mouse ID (string)
-         self.idx = 0
-         self.CID = ''  #Cage ID
-         self.ET = True    #Eartag?
-         self.sex = False   #False: Female, True: Male
-         self.age = ''       #str or int (depends), defaults to empty string
-         self.pregnant = False
-         self.sacked = ''   #Blank, may be sacked (Potential), or already sacked (Sacked)
-         self.DOS = ''     #Date of Sack
-         self.genotyped = False
-         self.runt = False
-         self.comment = ''  #Comment entry
+        self.ID = ID  # Mouse ID (string)
+        self.idx = 0
+        self.CID = ''  # Cage ID (string)
+        self.ET = True  # Eartag (bool)
+        self.sex = False  # False: Female, True: Male
+        self.age = ''  # str or int (status or age)
+        self.pregnant = False
+        self.sacked = ''  # Sack status
+        self.genotyped = False
+        self.runt = False
+
 
 class cage:
     def __init__(self, CID):
-        self.CID = CID   #Cage ID (string)
-        self.mice = []  #vector of mouse ID strings in this cage
-        self.status = ''
-        self.pups = 0   #Number of pups
-        self.DOB = ''   #Pup date of birth
+        self.CID = CID  # Cage ID (string)
+        self.mice = []  # vector of mouse IDs
+        self.status = ''  # status or X11 color name
+        self.pups = 0  # Number of pups
+        self.DOB = ''  # Pup date of birth
